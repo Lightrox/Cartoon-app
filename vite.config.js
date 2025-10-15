8bb5c504361424a3237f8c5771ab62e87c9ba43b
Aadarsh Tiwari <tiwariaadarsh830@gmail.com>
2025-10-15 11:40:03 +0530

    .

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Setting base to './' helps resolve asset paths correctly on Vercel
-  base: './'
 })
