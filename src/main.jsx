78b811b94237dfa45daf0c340645ab5c85bac4d7
Aadarsh Tiwari <tiwariaadarsh830@gmail.com>
2025-10-15 18:11:42 +0530

    Changes made in app.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'; // <-- Removed .jsx extension
+import App from './app.jsx'; // <-- Removed .jsx extension
 
 // This is the main entry point that renders the App component into the 'root' element
 ReactDOM.createRoot(document.getElementById('root')).render(
