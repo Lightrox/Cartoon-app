a2efcb61e75f821229569d81dec51cd91553b0f6
Aadarsh Tiwari <tiwariaadarsh830@gmail.com>
2025-10-15 11:38:38 +0530

    Changed

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,9 @@
-[Immersive content redacted for brevity.]
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // Setting base to './' helps resolve asset paths correctly on Vercel
+  base: './'
+})
